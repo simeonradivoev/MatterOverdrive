c6994e830f03e284902b7e49f6a6ac45ba0e80a0
simeonradivoev <simeonradivoev@gmail.com>
2015-04-06 09:46:24 +0300

    Aded slots to Patter Storage GUI

--- src/main/java/com/MO/MatterOverdrive/gui/GuiPatternStorage.java
+++ src/main/java/com/MO/MatterOverdrive/gui/GuiPatternStorage.java
@@ -3,11 +3,14 @@ package com.MO.MatterOverdrive.gui;
 import cofh.lib.gui.element.ElementEnergyStored;
 import com.MO.MatterOverdrive.Reference;
 import com.MO.MatterOverdrive.container.ContainerPatternStorage;
+import com.MO.MatterOverdrive.container.slot.SlotPatternStorage;
 import com.MO.MatterOverdrive.data.inventory.Slot;
 import com.MO.MatterOverdrive.gui.element.ElementPlayerSlots;
+import com.MO.MatterOverdrive.gui.element.ElementSlot;
 import com.MO.MatterOverdrive.gui.element.ElementSlotsList;
 import com.MO.MatterOverdrive.tile.TileEntityMachinePatternStorage;
 import net.minecraft.entity.player.InventoryPlayer;
+import org.lwjgl.opengl.GL11;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,6 +35,33 @@ public class GuiPatternStorage extends MOGuiBase
         slotsList = new ElementSlotsList(this,5,49,slots,0);
         playerSlots = new ElementPlayerSlots(this,44,91);
         this.patternStorage = patternStorage;
+    }
+
+    @Override
+    protected void drawGuiContainerBackgroundLayer(float partialTick, int x, int y) {
+
+        super.drawGuiContainerBackgroundLayer(partialTick,x,y);
+
+        DrawInputSlots();
+    }
+
+    public void DrawInputSlots()
+    {
+        GL11.glPushMatrix();
+        GL11.glTranslatef(guiLeft, guiTop, 0.0F);
+
+        this.bindTexture(ElementSlot.slot_big);
+
+        for (int x = 0; x < 3;x++)
+        {
+            this.drawSizedTexturedModalRect(x * 24 + 74, 34, 0, 0, 22, 22, 22, 22);
+        }
+        for (int x = 0; x < 3;x++)
+        {
+            this.drawSizedTexturedModalRect(x * 24 + 74, 24 + 34, 0, 0, 22, 22, 22, 22);
+        }
+
+        GL11.glPopMatrix();
     }
 
     @Override
