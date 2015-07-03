dafc1f4206de326ce8e4cd1ba5cc2d554b0b0dab
Simeon Radivoev <simeonradivoev@gmail.com>
2015-07-04 00:27:28 +0300

    Fixed Packet Server Side Crash
    Fixes #72.
    Should be happening by a ClientPacket is called by the server and messes things up,
    so I used the player's world object instead of the Minecraft.theWorld.

--- src/main/java/matteroverdrive/network/packet/client/starmap/PacketUpdateGalaxy.java
+++ src/main/java/matteroverdrive/network/packet/client/starmap/PacketUpdateGalaxy.java
@@ -7,7 +7,6 @@ import matteroverdrive.starmap.data.Galaxy;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import io.netty.buffer.ByteBuf;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 
 /**
