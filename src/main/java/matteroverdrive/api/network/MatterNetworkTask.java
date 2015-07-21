0edf1dd041d2f66c6867ed38ffd404e6162e459c
simeonradivoev <simeonradivoev@gmail.com>
2015-07-21 23:01:45 +0300

    Added default state for Task or null troubles.

--- src/main/java/matteroverdrive/api/network/MatterNetworkTask.java
+++ src/main/java/matteroverdrive/api/network/MatterNetworkTask.java
@@ -1,7 +1,24 @@
+/*
+ * This file is part of Matter Overdrive
+ * Copyright (c) 2015., Simeon Radivoev, All rights reserved.
+ *
+ * Matter Overdrive is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Matter Overdrive is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Matter Overdrive.  If not, see <http://www.gnu.org/licenses>.
+ */
+
 package matteroverdrive.api.network;
 
 import cofh.lib.util.position.BlockPosition;
-import matteroverdrive.Reference;
 import matteroverdrive.util.MOStringHelper;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
@@ -22,7 +39,7 @@ public abstract class MatterNetworkTask
     boolean isAlive;
 
     String unlocalizedName;
-    MatterNetworkTaskState state;
+    MatterNetworkTaskState state = MatterNetworkTaskState.UNKNOWN;
 
     public MatterNetworkTask()
     {
