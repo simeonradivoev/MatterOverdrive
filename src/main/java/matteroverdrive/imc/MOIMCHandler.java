cb48740a2622bba43d57ab6612f73ae318d2cea8
simeonradivoev <simeonradivoev@gmail.com>
2015-07-21 14:43:28 +0300

    Fixed the blacklist IMC handler to use the new MatterRegistry instance

--- src/main/java/matteroverdrive/imc/MOIMCHandler.java
+++ src/main/java/matteroverdrive/imc/MOIMCHandler.java
@@ -1,8 +1,26 @@
+/*
+ * This file is part of Matter Overdrive
+ * Copyright (c) 2015., Simeon Radivoev, All rights reserved.
+ *
+ * Matter Overdrive is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Matter Overdrive is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Matter Overdrive.  If not, see <http://www.gnu.org/licenses>.
+ */
+
 package matteroverdrive.imc;
 
 import cpw.mods.fml.common.event.FMLInterModComms;
+import matteroverdrive.MatterOverdrive;
 import matteroverdrive.api.IMC;
-import matteroverdrive.handler.MatterRegistry;
 
 /**
  * @author shadowfacts
@@ -16,7 +34,7 @@ public class MOIMCHandler {
 	public static void handleMessage(FMLInterModComms.IMCMessage msg) {
 		switch (msg.key) {
 			case IMC.MATTER_REGISTRY_BLACKLIST:
-				MatterRegistry.addToBlacklist(msg.getItemStackValue());
+				MatterOverdrive.matterRegistry.addToBlacklist(msg.getItemStackValue());
 		}
 	}
 
