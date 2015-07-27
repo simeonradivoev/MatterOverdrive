1e9a2596f7b98e13790453066c50870e635a2165
simeonradivoev <simeonradivoev@gmail.com>
2015-07-27 13:01:33 +0300

    Added clamping on Rouge Android Attributes To make sure that values don't go out of limits if the sharedMonsterAttributes have been modified.

--- src/main/java/matteroverdrive/entity/EntityRougeAndroidMob.java
+++ src/main/java/matteroverdrive/entity/EntityRougeAndroidMob.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of Matter Overdrive
+ * Copyright (c) 2015., Simeon Radivoev, All rights reserved.
+ *
+ * Matter Overdrive is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Matter Overdrive is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Matter Overdrive.  If not, see <http://www.gnu.org/licenses>.
+ */
+
 package matteroverdrive.entity;
 
 import matteroverdrive.entity.ai.AndoidTargetSelector;
@@ -42,9 +60,9 @@ public class EntityRougeAndroidMob extends EntityMob
     protected void applyEntityAttributes()
     {
         super.applyEntityAttributes();
-        this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(64.0D);
-        this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(1.6D);
-        this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(4.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(SharedMonsterAttributes.maxHealth.clampValue(64.0D));
+        this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(SharedMonsterAttributes.movementSpeed.clampValue(1.6D));
+        this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(SharedMonsterAttributes.attackDamage.clampValue(4.0D));
     }
 
     @Override
