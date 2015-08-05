9c7cf1f00b7475482347437ceef1d42a2e362c09
simeonradivoev <simeonradivoev@gmail.com>
2015-08-06 01:59:04 +0300

    Fixed Crashing while putting an item itemInventoryWrapper in weapon station Fixed crash for weapons that extend itemInventoryWrapper when put for the first time, without being used to instantiate NBT in Survival. Creates new NBT data if missing.

--- src/main/java/matteroverdrive/data/ItemInventoryWrapper.java
+++ src/main/java/matteroverdrive/data/ItemInventoryWrapper.java
@@ -1,9 +1,28 @@
+/*
+ * This file is part of Matter Overdrive
+ * Copyright (c) 2015., Simeon Radivoev, All rights reserved.
+ *
+ * Matter Overdrive is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Matter Overdrive is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Matter Overdrive.  If not, see <http://www.gnu.org/licenses>.
+ */
+
 package matteroverdrive.data;
 
 import matteroverdrive.util.MOInventoryHelper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 
 /**
  * Created by Simeon on 4/14/2015.
@@ -46,6 +65,9 @@ public class ItemInventoryWrapper implements IInventory
     @Override
     public void setInventorySlotContents(int slot, ItemStack stack)
     {
+        if (inventory.getTagCompound() == null)
+            inventory.setTagCompound(new NBTTagCompound());
+
         MOInventoryHelper.setInventorySlotContents(inventory,slot,stack);
     }
 
