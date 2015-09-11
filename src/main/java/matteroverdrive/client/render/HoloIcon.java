4a335915ae76bd6208317fa09a709c226a6df9a3
simeonradivoev <simeonradivoev@gmail.com>
2015-09-11 21:01:13 +0300

    Made Holo Icons store original size. Now even if Holo Icons are re-textured with larger size the original size will remain.

---
+++ src/main/java/matteroverdrive/client/render/HoloIcon.java
@@ -0,0 +1,58 @@
+/*
+ * This file is part of Matter Overdrive
+ * Copyright (c) 2015., Simeon Radivoev, All rights reserved.
+ *
+ * Matter Overdrive is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Matter Overdrive is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Matter Overdrive.  If not, see <http://www.gnu.org/licenses>.
+ */
+
+package matteroverdrive.client.render;
+
+import net.minecraft.util.IIcon;
+
+/**
+ * Created by Simeon on 8/28/2015.
+ */
+public class HoloIcon
+{
+    private IIcon icon;
+    private int originalWidth;
+    private int originalHeight;
+
+    public HoloIcon(IIcon icon,int originalX,int originalY)
+    {
+        this.icon = icon;
+        setOriginalSize(originalX,originalY);
+    }
+
+    public void setOriginalSize(int originalX,int originalY)
+    {
+        this.originalWidth = originalX;
+        this.originalHeight = originalY;
+    }
+
+    public int getOriginalWidth()
+    {
+        return originalWidth;
+    }
+
+    public int getOriginalHeight()
+    {
+        return originalHeight;
+    }
+
+    public IIcon getIcon()
+    {
+        return icon;
+    }
+}
--- src/main/java/matteroverdrive/client/render/HoloIcons.java
+++ src/main/java/matteroverdrive/client/render/HoloIcons.java
@@ -32,65 +32,120 @@ import net.minecraft.util.IIcon;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.Vec3;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Simeon on 6/15/2015.
  */
-public class HoloIcons
-{
+public class HoloIcons {
+    Map<String, HoloIcon> iconMap;
     public TextureMap textureMap;
     public int SheetSize = 256;
-    public ResourceLocation sheet = new ResourceLocation(Reference.MOD_ID,"textures/gui/holo_icons.png");
+    public ResourceLocation sheet = new ResourceLocation(Reference.MOD_ID, "textures/gui/holo_icons.png");
 
-    public String icons[] = new String[]{"holo_home","holo_dotted_circle","holo_factory","holo_galaxy","person",
-            "android_slot_arms","android_slot_chest","android_slot_head",
-            "android_slot_legs","android_slot_other","barrel","battery","color","factory","module","sights",
-            "shielding","scanner","upgrade","decompose","pattern_storage","home_icon","page_icon_home",
-            "page_icon_tasks","page_icon_upgrades","page_icon_config","page_icon_search","page_icon_info",
-            "page_icon_galaxy","page_icon_quadrant","page_icon_star","page_icon_planet","energy","arrow_right",
-            "travel_icon","icon_search","icon_size","icon_shuttle","icon_stats","icon_scount_planet","icon_attack",
-            "up_arrow","crosshair","up_arrow_large","android_feature_icon_bg","android_feature_icon_bg_active",
-            "health","black_circle","connections","ammo","temperature","flash_drive","trade","mini_quit","mini_back",
-            "tick"};
-
-
-    public HoloIcons()
-    {
-        textureMap = new TextureMap(4,"textures/gui/items");
-        Minecraft.getMinecraft().renderEngine.loadTextureMap(sheet,textureMap);
+    public HoloIcons() {
+        iconMap = new HashMap<>();
+        textureMap = new TextureMap(4, "textures/gui/items");
+        Minecraft.getMinecraft().renderEngine.loadTextureMap(sheet, textureMap);
     }
 
-    public void registerIcons(TextureMap textureMap)
-    {
-        for (String icon : icons)
-        {
-            textureMap.registerIcon(Reference.MOD_ID + ":" + icon);
-        }
+    public void registerIcons(TextureMap textureMap) {
+        reg("holo_home", 14);
+        reg("holo_dotted_circle", 20);
+        reg("holo_factory", 14);
+        //reg("holo_galaxy",14,14);
+        reg("person", 18);
+        reg("android_slot_arms", 16);
+        reg("android_slot_chest", 16);
+        reg("android_slot_head", 16);
+        reg("android_slot_legs", 16);
+        reg("android_slot_other", 16);
+        reg("barrel", 16);
+        reg("battery", 16);
+        reg("color", 16);
+        reg("factory", 16);
+        reg("module", 16);
+        reg("sights", 16);
+        reg("shielding", 16);
+        reg("scanner", 16);
+        reg("upgrade", 16);
+        reg("decompose", 16);
+        reg("pattern_storage", 16);
+        reg("home_icon", 14);
+        reg("page_icon_home", 14);
+        reg("page_icon_tasks", 15);
+        reg("page_icon_upgrades", 12);
+        reg("page_icon_config", 16);
+        reg("page_icon_search", 16);
+        reg("page_icon_info", 16);
+        reg("page_icon_galaxy", 11);
+        reg("page_icon_quadrant", 9);
+        reg("page_icon_star", 12);
+        reg("page_icon_planet", 16);
+        reg("energy", 16);
+        reg("arrow_right", 19);
+        reg("travel_icon", 18);
+        reg("icon_search", 16);
+        reg("icon_size", 16);
+        reg("icon_shuttle", 16);
+        reg("icon_size", 16);
+        reg("icon_stats", 16);
+        reg("icon_scount_planet", 32);
+        reg("icon_attack", 16);
+        reg("up_arrow", 7);
+        reg("crosshair", 3);
+        reg("up_arrow_large", 18);
+        reg("android_feature_icon_bg", 22);
+        reg("android_feature_icon_bg_active", 22);
+        reg("health", 18);
+        reg("black_circle", 18);
+        reg("connections", 16);
+        reg("ammo", 18);
+        reg("temperature", 18);
+        reg("flash_drive", 16);
+        reg("trade", 16);
+        reg("mini_quit", 16);
+        reg("mini_back", 16);
+        reg("tick", 16);
+        reg("list", 16);
+        reg("grid", 16);
+        reg("sort_random", 16);
+        reg("minimap_target",21);
 
         MatterOverdrive.statRegistry.registerIcons(textureMap);
     }
 
-    public IIcon getIcon(String icon)
-    {
-        return textureMap.getAtlasSprite(Reference.MOD_ID + ":" + icon);
+    private void reg(String iconName, int originalSize) {
+        iconMap.put(iconName, new HoloIcon(textureMap.registerIcon(Reference.MOD_ID + ":" + iconName), originalSize, originalSize));
     }
 
-    public void bindSheet()
-    {
+    public HoloIcon getIcon(String icon) {
+        return iconMap.get(icon);
+    }
+
+    public void bindSheet() {
         Minecraft.getMinecraft().renderEngine.bindTexture(sheet);
     }
 
-    public void renderIcon(String name,int x,int y)
-    {
-        bindSheet();
-        IIcon icon = getIcon(name);
-        RenderHelper.renderIcon(x, y, 0, icon, icon.getIconWidth(), icon.getIconHeight());
+    public void renderIcon(String name, int x, int y) {
+        HoloIcon icon = getIcon(name);
+        renderIcon(icon, x, y, icon.getOriginalWidth(), icon.getOriginalHeight());
     }
 
-    public void renderIcon(String name,int x,int y,int width,int height)
+    public void renderIcon(String name, int x, int y, int width, int height) {
+        renderIcon(getIcon(name), x, y, width, height);
+    }
+
+    public void renderIcon(HoloIcon icon, int x, int y)
+    {
+        renderIcon(icon,x,y,icon.getOriginalWidth(),icon.getOriginalHeight());
+    }
+
+    public void renderIcon(HoloIcon icon, int x, int y, int width, int height)
     {
         bindSheet();
-        IIcon icon = getIcon(name);
-        RenderHelper.renderIcon(x,y,0,icon,width,height);
+        RenderHelper.renderIcon(x,y,0,icon.getIcon(),width,height);
     }
 
     public static void tessalateParticleIcon(IIcon icon,double x,double y,double z,float size,GuiColor color)
