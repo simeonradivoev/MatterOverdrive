4fca8b776ad91026d55831d7efa71358f09f4e6d
simeonradivoev <simeonradivoev@gmail.com>
2015-09-28 18:52:03 +0300

    Added command for registering and blacklisting items in the matter registry.

--- src/main/java/matteroverdrive/commands/MatterRegistryCommands.java
+++ src/main/java/matteroverdrive/commands/MatterRegistryCommands.java
@@ -19,9 +19,14 @@
 package matteroverdrive.commands;
 
 import matteroverdrive.MatterOverdrive;
+import matteroverdrive.handler.ConfigurationHandler;
 import matteroverdrive.init.MatterOverdriveMatter;
 import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -50,13 +55,94 @@ public class MatterRegistryCommands extends CommandBase
     @Override
     public void processCommand(ICommandSender commandSender, String[] parameters)
     {
-        if (parameters.length > 0)
+        if (parameters.length == 1)
         {
             if (parameters[0].equalsIgnoreCase("recalculate"))
             {
                 MatterOverdrive.matterRegistry.getEntries().clear();
                 MatterOverdriveMatter.registerBasic(MatterOverdrive.configHandler);
                 MatterOverdrive.matterRegistrationHandler.runCalculationThread();
+            }
+        }else if (parameters.length == 2)
+        {
+            if (parameters[0].equalsIgnoreCase("blacklist"))
+            {
+                ItemStack stack;
+                if (parameters.length >= 4) {
+                    stack = getPlayer(commandSender, parameters[3]).getCurrentEquippedItem();
+                }else
+                {
+                    stack = getPlayer(commandSender,commandSender.getCommandSenderName()).getCurrentEquippedItem();
+                }
+
+                String key;
+                if (stack != null) {
+                    if (parameters[1].equalsIgnoreCase("itemstack")) {
+                        key = MatterOverdrive.matterRegistry.getKey(stack);
+
+                    } else if (parameters[1].equalsIgnoreCase("item")) {
+                        key = MatterOverdrive.matterRegistry.getKey(stack.getItem());
+                    } else if (parameters[1].equalsIgnoreCase("ore"))
+                    {
+                        key = parameters[2];
+                    }
+                    else {
+                        throw new CommandException("invalid type of item", new Object[]{parameters[1]});
+                    }
+
+                    MatterOverdrive.matterRegistry.addToBlacklist(key);
+                    String[] oldBlacklist = MatterOverdrive.configHandler.getStringList(ConfigurationHandler.CATEGORY_MATTER,ConfigurationHandler.KEY_MBLACKLIST);
+                    String[] newBlacklist = new String[oldBlacklist != null ? oldBlacklist.length+1 : 1];
+                    newBlacklist[oldBlacklist.length] = key;
+                    MatterOverdrive.configHandler.config.get(ConfigurationHandler.CATEGORY_MATTER,ConfigurationHandler.KEY_MBLACKLIST,new String[]{},"").set(newBlacklist);
+                    MatterOverdrive.configHandler.save();
+                    commandSender.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "["+key+"]" + EnumChatFormatting.RESET + " Added $s to matter blacklist and config.\nYou must recalculate the registry for changes to take effect.\nUse /matter_registry recalculate."));
+                }else
+                {
+                    throw new CommandException("player not holding any item", new Object[]{parameters[1]});
+                }
+            }
+        }
+        else if (parameters.length >= 3)
+        {
+            if (parameters[0].equalsIgnoreCase("register"))
+            {
+                int matter = parseInt(commandSender,parameters[2]);
+                ItemStack stack;
+                if (parameters.length >= 4) {
+                    stack = getPlayer(commandSender, parameters[3]).getCurrentEquippedItem();
+                }else
+                {
+                    stack = getPlayer(commandSender,commandSender.getCommandSenderName()).getCurrentEquippedItem();
+                }
+
+                if (stack != null) {
+                    String key;
+                    if (parameters[1].equalsIgnoreCase("itemstack"))
+                    {
+                        key = MatterOverdrive.matterRegistry.getKey(stack);
+
+
+                    } else if (parameters[1].equalsIgnoreCase("item")) {
+                        key = MatterOverdrive.matterRegistry.getKey(stack.getItem());
+                    }
+                    else if (parameters[1].equalsIgnoreCase("ore"))
+                    {
+                        key = parameters[2];
+                    }
+                    else
+                    {
+                        throw new CommandException("invalid type of item", new Object[]{parameters[1]});
+                    }
+
+                    MatterOverdrive.matterRegistry.register(key, matter);
+                    MatterOverdrive.configHandler.setInt(key, ConfigurationHandler.CATEGORY_NEW_ITEMS,matter);
+                    MatterOverdrive.configHandler.save();
+                    commandSender.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "["+key+"]"+ EnumChatFormatting.RESET +" Added $s to matter registry and config.\nYou can now recalculated the registry.\nUse /matter_registry recalculate."));
+                }else
+                {
+                    throw new CommandException("player not holding any item", new Object[]{parameters[1]});
+                }
             }
         }
     }
@@ -69,8 +155,13 @@ public class MatterRegistryCommands extends CommandBase
         if (parameters.length == 1)
         {
             commands.add("recalculate");
-            commands.add("add");
+            commands.add("register");
             commands.add("blacklist");
+        }else if (parameters.length == 2 && (parameters[0].equalsIgnoreCase("register") || parameters[0].equalsIgnoreCase("blacklist")))
+        {
+            commands.add("itemstack");
+            commands.add("item");
+            commands.add("ore");
         }
         return commands;
     }
