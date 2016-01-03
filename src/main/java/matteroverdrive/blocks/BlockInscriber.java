5e47e49da63400ac3620b3c41fccec03c2b2fc6e
simeonradivoev <simeonradivoev@gmail.com>
2016-01-03 16:56:20 +0200

    Fixes #370

--- src/main/java/matteroverdrive/blocks/BlockInscriber.java
+++ src/main/java/matteroverdrive/blocks/BlockInscriber.java
@@ -35,6 +35,9 @@ public class BlockInscriber extends MOBlockMachine
 {
     public BlockInscriber(Material material, String name) {
         super(material, name);
+        setHardness(20.0F);
+        this.setResistance(9.0f);
+        this.setHarvestLevel("pickaxe",2);
         setHasGui(true);
     }
 
