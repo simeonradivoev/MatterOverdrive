e51d5b3c59a2badbaa3caaa52c53cc3ccc1c70e3
Shadowfacts <me@shadowfacts.net>
2016-07-30 11:06:43 -0400

    Version replacement

--- src/main/java/matteroverdrive/Reference.java
+++ src/main/java/matteroverdrive/Reference.java
@@ -25,7 +25,7 @@ public class Reference
 {
 	public static final String MOD_ID				= "mo";
 	public static final String MOD_NAME				= "Matter Overdrive";
-	public static final String VERSION				= "0.4.3";
+	public static final String VERSION				= "@VERSION@";
 	public static final String VERSION_DATE			= "07.04.2016";
 	public static final String GUI_FACTORY_CLASS	= "matteroverdrive.gui.GuiConfigFactory";
 	public static final String DEPEDNENCIES			= "required-after:shadowmc@[3.4.5-SNAPSHOT,);required-after:Tesla@[1.2.0.38,);after:Waila;after:theoneprobe;";
